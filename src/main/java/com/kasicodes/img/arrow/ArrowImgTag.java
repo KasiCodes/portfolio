473a57837eaba7068252053fda1a0b442634d74c
mrampai023 <mrampai023@student.wethinkcode.co.za>
2024-11-19 16:25:39 +0200

    added image dir

---
+++ src/main/java/com/kasicodes/img/arrow/ArrowImgTag.java
@@ -0,0 +1,11 @@
+package com.kasicodes.img.arrow;
+
+import com.kasicodes.img.ImgTagAttributes;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder(toBuilder = true)
+public class ArrowImgTag extends ImgTagAttributes {
+}
